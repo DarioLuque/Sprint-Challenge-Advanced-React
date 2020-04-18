66ec14b8704e00f2ba3f23e53fffe5fecefb000b
Dario Luque <dario.lu92@gmail.com>
2020-04-18 12:21:40 -0700

    Added custom hook Dark Mode

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import './styles.css'
 
-export default function Header() {
+import {useDarkMode} from "../hooks/useDarkMode"
 
-  return(
+const Header = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+    console.log(darkMode)
+  };
+
+  
+  return (
     <HeaderTop>
       <h1>Advanced React - Women's World Cup</h1>
+      <div className="dark-mode__toggle">
+        <div
+          onClick={toggleMode}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
+        />
+      </div>
     </HeaderTop>
   );
-}
+};
+
+export default Header;
 
 //styles
 
 const HeaderTop = styled.div`
 width: 100%;
-background: white;
+background: black;
+color: white;
 text-align: center;
 font-family: 'Anton', sans-serif;
 `;
